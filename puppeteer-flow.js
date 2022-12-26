7eb425b7a14e7160c1fbcb795a7be8ca39a99834
flow <git@lowspace.de>
2022-12-26 01:35:11 +0100

    setup

--- puppeteer-flow.js
+++ puppeteer-flow.js
@@ -4,6 +4,9 @@ const microtime = require("microtime");
 
 let sleep = ms => new Promise(r => setTimeout(r, ms));
 
+// Select quality
+const quality = '1080p';
+
 (async () => {
   console.log(microtime.now(), " Launching Browser");
   const browser = await puppeteer.launch({
@@ -51,8 +54,6 @@ let sleep = ms => new Promise(r => setTimeout(r, ms));
     await sleep(500);
   });
 
-  // Select quality
-  const quality = '480p';
   let selection;
   let qualityOptions = await page.$$(".ytp-menuitem")
   let qualityOptionsValues = await Promise.all(qualityOptions.map(async (el) => await (await el.getProperty('innerText')).jsonValue()));
